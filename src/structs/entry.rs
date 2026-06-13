923aa9707c4da27520be6e5124fa3c7899a1b134
Konataware <luansiloe404@gmail.com>
2026-06-13 17:57:48 -0300

    Entry struct

---
+++ src/structs/entry.rs
@@ -0,0 +1,30 @@
+use serde::{Deserialize, Serialize};
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct Entry {
+    // id, name, username, password
+    id: Option<u32>,
+    name: String,
+    filename: String
+}
+
+/*
+ * REVISE!
+ * I need to properly consider how to format this request to the python bridge.
+ * A data field composed of a single JSON blob would probably be the best solution to it
+ * There is no reason to structure data because it won't be stored anywhere
+*/
+impl Entry {
+    pub fn new(name: String, filename: String) -> Self {
+        Self {
+            id: None, //I can't safely create an ID until i know our current id cap.
+            name,
+            filename,
+
+        }
+    }
+
+    pub fn set_id(&mut self, id: Option<u32>) {
+        self.id = id;
+    }
+}
